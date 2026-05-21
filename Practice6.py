fc23b26bd5acba6240f1573b91282e6aba68ab1e
sharikhn545@gmail.com <sharikhn545@gmail.com>
2026-05-21 19:54:26 +0530

    final commit of day

--- Practice6.py
+++ Practice6.py
@@ -6,13 +6,39 @@ logs = [
  {"endpoint": "/api/payments", "status": 200},
  {"endpoint": "/api/payments", "status": 500}
 ]
-c={{}}
-for i in logs:
-    b=i.get("endpoint")
-    d=i.get("status")
-    if b not in c:
-        c.
-        print(c)
+# logs=[]
+# c={}
+# for i in logs:
+#     b=i.get("endpoint")
+#     endpoint=b.lower()
+#     status=i.get("status")
+#     # print(endpoint)
+#     if endpoint is None or status is None:
+#         print("UNKNOWN")
+#     else:
+#         if endpoint not in c:
+#             c.update({endpoint:{}})
+#         f=c.get(endpoint)
+#         if status not in f:
+#             f.update({status:1})
+#         else:
+#             f[status]=f[status]+1
+# print(c)
+
+warehouse_a = {
+ "p101": {"stock": 50, "price": 19.99},
+ "p102": {"stock": 10, "price": 45.00}
+}
+warehouse_b = {
+ "p102": {"stock": 15, "price": 40.00},
+ "p103": {"stock": 100, "price": 5.99}
+}
+result={}
+a=warehouse_a.keys()
+b=warehouse_b.keys()
+if a in b:
+    result.update(warehouse_a.values())
     
-        
+    
+
         
